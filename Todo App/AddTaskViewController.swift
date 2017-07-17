66059617a5173e6604e71422ba3ec50c068e530e
Juan Naputi <jnaputi253@gmail.com>
2017-07-16 20:07:54 -0700

    Made ViewControllers for each View and placed them in a folder

---
+++ Todo App/AddTaskViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddTaskViewController.swift
+//  Todo App
+//
+//  Created by Juan Naputi on 7/16/17.
+//  Copyright © 2017 Juan Naputi. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("AddTask View Controller loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Todo App/TodoListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TodoListViewController.swift
+//  Todo App
+//
+//  Created by Juan Naputi on 7/16/17.
+//  Copyright © 2017 Juan Naputi. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("TodoList View Controller loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
